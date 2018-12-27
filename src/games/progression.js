8130204b19999ac670431a0885400a2635da9483
bncld <bonny.monet@gmail.com>
2018-12-27 17:06:03 +0200

    исправил свои недочеты 7 этапа

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,18 +6,18 @@ const description = 'What number is missing in the progression?';
 const progressionLength = 10;
 
 const getGameData = () => {
-  const start = randomNumber(1, 20);
-  const step = randomNumber(1, 20);
+  const start = randomNumber(1, 10);
+  const step = randomNumber(1, 10);
   const hiddenNumber = randomNumber(1, 10);
-  const question = ``;
+  let question = ' ';
   const correctAnswer = hiddenNumber * step + start;
   for (let i = 1; i < progressionLength; i += 1) {
     const currentNumber = start + step * i;
     if (currentNumber === correctAnswer) {
-      question += '..';
+      question += ' .. ';
     } else {
-      question += start + step * i;
-    } 
+      question += `${currentNumber} `;
+    }
   }
   return cons(question, String(correctAnswer));
 };
