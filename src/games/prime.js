d6abf84356651cafb9af86c0c0355a10202b523a
bncld <bonny.monet@gmail.com>
2018-12-28 12:25:44 +0200

    lint

--- src/games/prime.js
+++ src/games/prime.js
@@ -7,7 +7,7 @@ const description = 'Answer "yes" if given number is prime. Otherwise answer "no
 const isPrime = (number) => {
   const upperBorder = number / 2;
   if (number < 2) {
-      return false;
+    return false;
   }
   for (let i = 2; i <= upperBorder; i += 1) {
     if (number % i === 0) {
